56b7ff37ffab4ff4892c2937c774097a95877716
markshin <shin.dongyeon@gmail.com>
2018-03-07 18:00:49 +0900

    Calculator Init

---
+++ calculator.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int add(int a, int b); 
+int sub(int a, int b);
+int mul(int a, int b);
+
+void main(){
+
+  int n, m;
+  char op;
+
+  while(1){
+    scanf("%d %c %d", &n, &op, &m);
+    switch(op) {
+    case '+' : printf("%d %c %d = %d\n", n, op, m, add(n, m)); return;
+    case '-' : printf("%d %c %d = %d\n", n, op, m, sub(n, m)); return;
+    case '*' : printf("%d %c %d = %d\n", n, op, m, mul(n, m)); return;
+    default : printf("invalid input\n"); break;
+    }
+  }
+
+}
+
+int add(int a, int b) {
+  return 0; // TODO : FIX IT
+}
+
+int sub(int a, int b) {
+  return 0; // TODO : FIX IT
+}
+
+int mul(int a, int b) {
+  return 0; // TODO : FIX IT
+}
